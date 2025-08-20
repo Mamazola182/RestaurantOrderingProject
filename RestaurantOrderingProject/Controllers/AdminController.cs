90ea3c446180c236d38db1b1eb166124994ea69d
Thao <oahtneik@gmail.com>
2025-08-20 09:28:53 +0700

    Add boostrap, create Admin layout

---
+++ RestaurantOrderingProject/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrderingProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /Admin/Dashboard
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        // GET: /Admin/Subjects
+        public IActionResult Subjects()
+        {
+            return View();
+        }
+    }
+}
